7f02c85159a3d4fde04b56580109e5c7e13f14d9
Aldwin Su <fangyansu@163.com>
2018-07-09 15:53:19 +0800

    Add load method in WorkbookUtils.

--- src/main/java/devutility/external/poi/utils/SheetUtils.java
+++ src/main/java/devutility/external/poi/utils/SheetUtils.java
@@ -1,5 +1,18 @@
 package devutility.external.poi.utils;
 
-public class SheetUtils {
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.WorkbookUtil;
 
+public class SheetUtils {
+	/**
+	 * Create a Sheet object with unsafe name.
+	 * @param workbook: Workbook object.
+	 * @param name: Sheet name.
+	 * @return Sheet
+	 */
+	public static Sheet createSheet(Workbook workbook, String name) {
+		String safeName = WorkbookUtil.createSafeSheetName(name);
+		return workbook.createSheet(safeName);
+	}
 }
--- src/main/java/devutility/external/poi/utils/WorkbookUtils.java
+++ src/main/java/devutility/external/poi/utils/WorkbookUtils.java
@@ -1,18 +1,29 @@
 package devutility.external.poi.utils;
 
-import org.apache.poi.ss.usermodel.Sheet;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.util.WorkbookUtil;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class WorkbookUtils {
 	/**
-	 * Create a Sheet object with unsafe name.
-	 * @param workbook: Workbook object.
-	 * @param name: Sheet name.
-	 * @return Sheet
+	 * Load excel file with file path.
+	 * @param filePath: Excel file path.
+	 * @return Workbook object.
+	 * @throws EncryptedDocumentException
+	 * @throws InvalidFormatException
+	 * @throws IOException
 	 */
-	public static Sheet createSheet(Workbook workbook, String name) {
-		String safeName = WorkbookUtil.createSafeSheetName(name);
-		return workbook.createSheet(safeName);
+	public static Workbook load(String filePath) throws IOException, EncryptedDocumentException, InvalidFormatException {
+		File file = new File(filePath);
+
+		if (!file.exists()) {
+			throw new IOException(String.format("%s cannot found!", filePath));
+		}
+
+		return WorkbookFactory.create(file);
 	}
 }
