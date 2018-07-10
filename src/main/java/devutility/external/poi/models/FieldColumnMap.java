79f4de7b667dafd1561d12263bdd2e6a9f2cee56
Aldwin Su <fangyansu@163.com>
2018-07-10 18:18:19 +0800

    Update

--- src/main/java/devutility/external/poi/models/FieldColumnMap.java
+++ src/main/java/devutility/external/poi/models/FieldColumnMap.java
@@ -1,15 +1,55 @@
 package devutility.external.poi.models;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-public class FieldColumnMap {
+import devutility.internal.lang.ClassHelper;
+import devutility.internal.lang.models.EntityField;
+import devutility.internal.util.CollectionUtils;
+
+public class FieldColumnMap<T> {
+	/**
+	 * Type T Class object.
+	 */
+	private Class<T> clazz = null;
+
 	/**
 	 * Entries container
 	 */
 	private HashMap<String, FieldColumnEntry> map = new LinkedHashMap<>();
+
+	/**
+	 * EntityField objects for T.
+	 */
+	private List<EntityField> entityFields = new ArrayList<>();
+
+	public FieldColumnMap() {
+		Type type = getClass().getGenericSuperclass();
+
+		if (type instanceof ParameterizedType) {
+			ParameterizedType pType = (ParameterizedType) type;
+			Type claz = pType.getActualTypeArguments()[0];
+
+			if (claz instanceof Class) {
+				this.clazz = (Class<T>) claz;
+			}
+		}
+
+		entityFields = ClassHelper.getEntityFields(clazz);
+	}
+
+	/**
+	 * Find EntityField from entityFields.
+	 * @param field: Field name in model.
+	 * @return EntityField
+	 */
+	private EntityField findEntityField(String field) {
+		return CollectionUtils.find(entityFields, i -> field.equals(i.getField().getName()));
+	}
 
 	/**
 	 * Put a new or existed FieldColumnEntry object in map container.
@@ -18,6 +58,7 @@ public class FieldColumnMap {
 	 * @return FieldColumnEntry
 	 */
 	public synchronized FieldColumnEntry put(String modelField, int columnIndex) {
+
 		FieldColumnEntry entry = new FieldColumnEntry(modelField, columnIndex);
 		map.put(modelField, entry);
 		return entry;
---
+++ src/main/java/devutility/external/poi/utils/RowUtils.java
@@ -0,0 +1,16 @@
+package devutility.external.poi.utils;
+
+import org.apache.poi.ss.usermodel.Row;
+
+import devutility.external.poi.models.FieldColumnEntry;
+import devutility.external.poi.models.FieldColumnMap;
+import devutility.internal.lang.ClassHelper;
+
+public class RowUtils {
+
+	public static <T> T toModel(Row row, FieldColumnMap fieldColumnMap, Class<T> clazz) throws ReflectiveOperationException {
+		T model = ClassHelper.newInstance(clazz);
+
+		return null;
+	}
+}
