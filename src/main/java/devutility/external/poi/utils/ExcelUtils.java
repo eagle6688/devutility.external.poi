9fc155e3c8eaa2e8c77f82eaef4abb1b6769c88c
Aldwin Su <fangyansu@163.com>
2019-04-17 16:21:22 +0800

    Upgrade to version 1.0.2

--- src/main/java/devutility/external/poi/utils/ExcelUtils.java
+++ src/main/java/devutility/external/poi/utils/ExcelUtils.java
@@ -6,27 +6,27 @@ import devutility.internal.io.FileUtils;
 public class ExcelUtils {
 	/**
 	 * Is xls file.
-	 * @param fileName: File name
+	 * @param fileName File name
 	 * @return boolean
 	 */
 	public static boolean isXls(String fileName) {
-		String extension = FileUtils.getFileExtension(fileName).toLowerCase();
+		String extension = FileUtils.getExtension(fileName).toLowerCase();
 		return ".xls".equals(extension);
 	}
 
 	/**
 	 * Is xlsx file.
-	 * @param fileName: File name.
+	 * @param fileName File name.
 	 * @return boolean
 	 */
 	public static boolean isXlsx(String fileName) {
-		String extension = FileUtils.getFileExtension(fileName).toLowerCase();
+		String extension = FileUtils.getExtension(fileName).toLowerCase();
 		return ".xlsx".equals(extension);
 	}
 
 	/**
 	 * Get ExcelType by file name.
-	 * @param fileName: File name.
+	 * @param fileName File name.
 	 * @return ExcelType
 	 */
 	public static ExcelType getExcelType(String fileName) {
